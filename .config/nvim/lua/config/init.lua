f5b19e571def6fb48c5aedde00a523db5b665b72
Nadav Samet <thesamet@gmail.com>
2024-12-29 18:53:42 -0800

    updates

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,20 +1,22 @@
 require("config.lazy")
 
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup({})
 
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.opt.colorcolumn="100"
-vim.opt.ruler=true
-vim.opt.shiftwidth=2
-vim.opt.smartindent=true
-vim.opt.tabstop=2
-vim.opt.cursorline=true
-vim.opt.expandtab=true
+vim.opt.colorcolumn = "100"
+vim.opt.ruler = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.signcolumn = "number"
 
-vim.keymap.set('n', '<left>', ':bp<CR>', { silent = true })
-vim.keymap.set('n', '<right>', ':bn<CR>', { silent = true })
-vim.keymap.set('n', ',,', ':wincmd w<CR>', { silent = true })
-vim.keymap.set('n', ',.', ':wincmd W<CR>', { silent = true })
-vim.keymap.set('c', '<C-D>', "<C-r>=expand('%:h')<CR>/")
+vim.keymap.set("n", "<left>", ":bp<CR>", { silent = true })
+vim.keymap.set("n", "<right>", ":bn<CR>", { silent = true })
+vim.keymap.set("n", ",,", ":wincmd w<CR>", { silent = true })
+vim.keymap.set("n", ",.", ":wincmd W<CR>", { silent = true })
+vim.keymap.set("c", "<C-D>", "<C-r>=expand('%:h')<CR>/")

8de5e93a7a523ed9e917812973a7407b15e0d88b
Nadav Samet <thesamet@gmail.com>
2025-01-16 21:45:10 -0800

    add kitty scrollback

---
+++ .config/nvim/lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,15 @@
+return {
+	"mikesmithgh/kitty-scrollback.nvim",
+	enabled = true,
+	lazy = true,
+	cmd = {
+		"KittyScrollbackGenerateKittens",
+		"KittyScrollbackCheckHealth",
+		"KittyScrollbackGenerateCommandLineEditing",
+	},
+	event = { "User KittyScrollbackLaunch" },
+	version = "*", -- latest stable version, may have breaking changes if major version changed
+	config = function()
+		require("kitty-scrollback").setup()
+	end,
+}

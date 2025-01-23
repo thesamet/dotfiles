53a93502dd0c3d63b28ad436bab123165e53f2e2
Nadav Samet <thesamet@gmail.com>
2025-01-22 23:03:00 -0800

    neovim and neovide copy-paste

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -25,3 +25,18 @@ vim.keymap.set("n", ",n", function()
 	vim.lsp.inlay_hint.enable(enabled, { 0 })
 	vim.notify("Inlay hints: " .. (enabled and " on" or "off"))
 end, { buffer = 0, desc = "Toggle inlay hints" })
+
+if vim.g.neovide then
+	vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<C-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })

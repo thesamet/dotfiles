e9f61da22939b62a16a29a6f5cbd6f5d5be4ba62
Nadav Samet <thesamet@gmail.com>
2025-02-08 14:27:00 -0800

    nvim config

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -29,14 +29,14 @@ end, { buffer = 0, desc = "Toggle inlay hints" })
 if vim.g.neovide then
 	vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
 	vim.keymap.set("v", "<C-c>", '"+y') -- Copy
-	vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
-	vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
-	vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
+	vim.keymap.set("n", "<C-S-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<C-S-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<C-S-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<C-S-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 
 -- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
